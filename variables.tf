2e673201e986866c4aab521542726e65c1551434
Luan Pontes <luanpontes2@gmail.com>
2021-07-30 18:46:49 -0300

    ajuste inputs and outpus

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "cluster_name" {
-  default = "luan-cluster"
-}
-
-
 variable "aws_access_key" {
   
 }
@@ -13,53 +8,4 @@ variable "aws_secret_key" {
 
 variable "aws_region" {
   default = "us-east-1"  
-}
-
-variable "map_accounts" {
-  description = "Additional AWS account numbers to add to the aws-auth configmap."
-  type        = list(string)
-
-  default = [
-    "777777777777",
-    "888888888888",
-  ]
-}
-
-variable "map_roles" {
-  description = "Additional IAM roles to add to the aws-auth configmap."
-  type = list(object({
-    rolearn  = string
-    username = string
-    groups   = list(string)
-  }))
-
-  default = [
-    {
-      rolearn  = "arn:aws:iam::66666666666:role/role1"
-      username = "role1"
-      groups   = ["system:masters"]
-    },
-  ]
-}
-
-variable "map_users" {
-  description = "Additional IAM users to add to the aws-auth configmap."
-  type = list(object({
-    userarn  = string
-    username = string
-    groups   = list(string)
-  }))
-
-  default = [
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user1"
-      username = "user1"
-      groups   = ["system:masters"]
-    },
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user2"
-      username = "user2"
-      groups   = ["system:masters"]
-    },
-  ]
 }
